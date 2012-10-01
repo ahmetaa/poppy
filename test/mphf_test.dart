c9edef7179d29b290593f6d10d869c80fb3bf304
ahmetaa <ahmetaa@gmail.com>
2012-10-01 22:14:14 +0300

    add basic test. Fix for key size less than 3

---
+++ test/mphf_test.dart
@@ -0,0 +1,42 @@
+import 'package:unittest/unittest.dart';
+import 'package:poppy/mphf.dart';
+import 'dart:math';
+
+bool randomStringTest() {
+  var nums = [1,2,3,4,5,10,100,1000,1001,10000];
+  for(num size in nums) {
+    Set<String> strings = randomStrings(size, 7);
+    Mphf hash = new Mphf.fromStrings(strings);
+    var values = new Set<int>();
+    for(String s in strings) {
+      int value = hash.hashValue(s.charCodes());
+      if(values.contains(value)) {
+        print("Duplicated value $value for key $s for set size $size");
+        return false;
+      }
+      values.add(value);
+      return true;
+    }
+  }
+
+}
+
+Set<String> randomStrings(num AMOUNT, num LENGTH) {
+  var rnd = new Random();
+  var testVals = new Set<String>();
+  while(testVals.length < AMOUNT) {
+    var buffer = new StringBuffer();
+    for(int k = 0; k<LENGTH; k++) {
+      int randomChar = rnd.nextInt(26)+'a'.charCodeAt(0);
+      buffer.addCharCode(randomChar);
+    }
+    testVals.add(buffer.toString());
+  }
+  return testVals;
+}
+
+main() {
+  test('Random strings.', () {
+    expect(randomStringTest(), equals(true));
+  });
+}
