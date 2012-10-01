48e047a1e48fd6f8bc95c250137fae758bcbe2b9
ahmetaa <ahmetaa@gmail.com>
2012-10-01 13:30:54 +0300

    examples

---
+++ example/random_strings.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'package:poppy/mphf.dart';
+
+/*
+ * - Generate [AMOUNT] [LENGTH] legth unique strings.
+ * - Generate minimal perfect hash function for this string set.
+ * - Query all strings to check speed. Use unchecked mode for speed.
+ */
+  
+final num AMOUNT = 100000;
+final num LENGTH = 7;
+
+void main() {   
+  
+  print("Generating $AMOUNT amount of $LENGTH length unique strings"); 
+  var stopWatch = new Stopwatch()..start();
+  var testStrings = _randomStrings();
+  print("String set generation time : ${stopWatch.elapsedInMs()} ms \n");
+  
+  for(int k=0; k<5; k++) {
+    print("iteration: $k");
+    stopWatch..reset()..start();      
+    var hash = new Mphf.fromStrings(testStrings);
+    print("Hash generation time : ${stopWatch.elapsedInMs()} ms");
+    print("Average bit per key: ${hash.averageBitsPerKey()}");
+
+    List<List<int>> l = new List();
+    for(String s in testStrings) {
+      l.add(s.charCodes());
+    }
+    stopWatch..reset()..start();
+    for(int i = 0, length = l.length; i<length; ++i) {
+      int k = hash.hashValue(l[i]);
+    }
+    print("Hash query time : ${stopWatch.elapsedInMs()} ms \n");
+  }
+}
+
+Set<String> _randomStrings() {
+  var rnd = new Random();
+  var testVals = new Set<String>();
+  while(testVals.length < AMOUNT) {
+    var buffer = new StringBuffer();
+    for(int k = 0; k<LENGTH; k++) {
+      int randomChar = rnd.nextInt(26)+'a'.charCodeAt(0);
+      buffer.addCharCode(randomChar);
+    }
+    testVals.add(buffer.toString());
+  }
+  return testVals;
+}
+  
+
