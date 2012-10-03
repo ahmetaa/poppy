29a1d5b0653010c88601f28700d58fa55d6dc009
ahmetaa <ahmetaa@gmail.com>
2012-10-03 17:37:29 +0300

    eliminate unused import

--- example/fruits.dart
+++ example/fruits.dart
@@ -1,5 +1,4 @@
 import 'package:poppy/mphf.dart';
-import 'packages/poppy/src/fixed_bit_vector.dart';
 
 void main() {
   var fruits = ["apple", "orange", "blueberry", "cherry", "pomegranate", "plum", "pear"];
