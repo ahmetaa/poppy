6ceb398991fbaecf968f172fc48c50ce563bd1b1
mdakin <mdakin@gmail.com>
2012-10-03 10:47:52 +0200

    Fix leftover.

--- example/fruits.dart
+++ example/fruits.dart
@@ -2,14 +2,6 @@ import 'package:poppy/mphf.dart';
 import 'packages/poppy/src/fixed_bit_vector.dart';
 
 void main() {
-  FixedBitVector bv = new FixedBitVector.bitCount(1000000);
-  Stopwatch sv = new Stopwatch()..start();
-  for (int i = 0; i < 1000000; i++) {
-    bv.setBit(i);
-    bv.getBit(i);
-    bv.clear(i);
-  }
-  print("Time: ${sv.elapsedInMs()}");
   var fruits = ["apple", "orange", "blueberry", "cherry", "pomegranate", "plum", "pear"];
   var mphf = new Mphf.fromStrings(fruits);
   for(var fruit in fruits) {
