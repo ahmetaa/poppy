293af8056a665e22ff37a2cd2977eadbd67967ce
ahmetaa <ahmetaa@gmail.com>
2013-01-23 14:10:03 +0200

    test_runner.dart and necessary lib changes

---
+++ test/test_runner.dart
@@ -0,0 +1,17 @@
+import "package:unittest/unittest.dart";
+import "bloom_filter_test.dart" as bloom;
+import "int_set_test.dart" as intSet;
+import "sparse_vector_test.dart" as sparseVec;
+import "simhash_test.dart" as simHash;
+import "mphf_test.dart" as mphf;
+import "trie_test.dart" as trie;
+
+main() {
+  bloom.main();
+  intSet.main();
+  sparseVec.main();
+  simHash.main();
+  mphf.main();
+  trie.main(); 
+}
+
